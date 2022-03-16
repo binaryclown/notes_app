25f3235340c3461a0f5213c9af0bf5d11e105571
Aleksandr Zhirnov <ar.zhirnov@gmail.com>
2022-03-16 18:59:57 +0300

    First commit

---
+++ src/main/java/binaryclown/petprojects/rest/notesappserver/NotesAppServerApplication.java
@@ -0,0 +1,13 @@
+package binaryclown.petprojects.rest.notesappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/binaryclown/petprojects/rest/notesappserver/NotesAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package binaryclown.petprojects.rest.notesappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
